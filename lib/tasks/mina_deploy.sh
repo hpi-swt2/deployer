e12f4c269918c85d7fadebc29a017c816c40105f
Christoph Matthies <chrisma@users.noreply.github.com>
2018-11-22 15:55:10 +0100

    Add bin

--- lib/tasks/mina_deploy.sh
+++ lib/tasks/mina_deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 path="/home/hrmtadm/vm-portal"
 git --version && \
-$path/rails --version && \
+$path/bin/rails --version && \
 git -C $path ls-remote --get-url && \
 git -C $path pull && \
 BUNDLE_GEMFILE=$path/Gemfile $path/bin/bundle install && \
